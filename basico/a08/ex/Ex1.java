3c32cbc51ffacc90a7b1c4e5f9fd6fd1636afad8
NicolasEstudos048 <estudosnicolas2@gmail.com>
2024-07-09 20:42:09 -0300

    Pedindo numero ate ele estar entre 0 e 10

---
+++ basico/a08/ex/Ex1.java
@@ -0,0 +1,28 @@
+package basico.a08.ex;
+
+import java.util.Scanner;
+
+public class Ex1 {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) { 
+            
+            System.out.print("Num entre 0 e 10: ");
+            int num = scanner.nextInt();
+
+            System.out.println(num);
+
+            if (num >= 0 && num <= 10) {
+                break;
+            }
+
+        }
+
+
+
+    }
+
+}
