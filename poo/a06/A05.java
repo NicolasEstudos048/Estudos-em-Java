cac2bcf6054f4b031adfea973aa54121992ddb90
NicolasEstudos048 <estudosnicolas2@gmail.com>
2024-08-03 22:09:03 -0300

    Encapsulamento

---
+++ poo/a06/A05.java
@@ -0,0 +1,12 @@
+package poo.a06;
+
+public class A05 {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.maisVolume();
+        c.play();
+        c.abriMenu();
+        c.fecharMenu();
+    }
+}
