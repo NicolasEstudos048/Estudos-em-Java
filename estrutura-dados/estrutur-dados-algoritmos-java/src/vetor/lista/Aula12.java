b1c1094695c33a7cc69e804da09a8119d4e7297a
NicolasEstudos048 <estudosnicolas2@gmail.com>
2024-09-04 07:04:45 -0300

    API Java: ArrayList

---
+++ estrutura-dados/estrutur-dados-algoritmos-java/src/vetor/lista/Aula12.java
@@ -0,0 +1,40 @@
+package vetor.lista;
+
+import java.util.ArrayList;
+
+public class Aula12 {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<String>();
+
+        arrayList.add("A");
+        arrayList.add("C");
+        System.out.println(arrayList);
+
+        arrayList.add(1, "B");
+        System.out.println(arrayList);
+
+        boolean existe = arrayList.contains("A");
+        if (existe) {
+            System.out.println("Elemento existe!");
+        } else {
+            System.out.println("Elemento não existe!");
+        }
+
+        int posicao = arrayList.indexOf("D");
+        if (posicao > -1) {
+            System.out.println("Elemento existe");
+        } else {
+            System.out.println("Elemento não existe!");
+        }
+
+        // Retorna elemento da posição passada
+        System.out.println(arrayList.get(2));
+
+        arrayList.remove(0);
+        arrayList.remove("B");
+
+        System.out.println(arrayList);
+
+        System.out.println(arrayList.size());
+    }
+}
